239a11dbb2d854f133292d1f8c63d1a68ac27b7e
Derryck <oseiagyemanb@gmail.com>
2023-04-10 16:12:52 +0100

    initial commit

---
+++ slider/src/App.jsx
@@ -0,0 +1,13 @@
+import SlickCarousel from './SlickCarousel';
+import Carousel from './Carousel';
+
+const App = () => {
+   return (
+      <main>
+         {/* <Carousel /> */}
+         <SlickCarousel />
+      </main>
+   );
+};
+
+export default App;
