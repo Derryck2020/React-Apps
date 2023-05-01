784c3e400de02560c7bd7bd1e3cbe9d158bcb0dd
Derryck <oseiagyemanb@gmail.com>
2023-05-01 03:44:32 +0100

    update of files

--- pictures/src/Gallery.jsx
+++ pictures/src/Gallery.jsx
@@ -1,20 +1,61 @@
 import React, { useCallback } from 'react';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
+import { useGlobalContext } from './context';
 
 const url =
-   'https://api.unsplash.com/search/photos?client_id=TV8N-jvbvd9LHjYR6EOq34H-N5mg36wpGQTcj3rwsgs&query=office';
+   'https://api.unsplash.com/search/photos?client_id=TV8N-jvbvd9LHjYR6EOq34H-N5mg36wpGQTcj3rwsgs';
 
 const Gallery = () => {
+   const { searchTerm } = useGlobalContext();
    const response = useQuery({
-      queryKey: ['images'],
+      queryKey: ['images', searchTerm],
       queryFn: async () => {
-         const result = await axios.get(url);
+         const result = await axios.get(`${url}&query=${searchTerm}`);
+
+         console.log(result);
          return result.data;
       },
    });
-   console.log(response);
-   return <h2>Gallery</h2>;
+   if (response.isLoading) {
+      return (
+         <section className="image-container">
+            <h4>Loading...</h4>
+         </section>
+      );
+   }
+   if (response.isError) {
+      return (
+         <section className="image-container">
+            <h4>There was an error....</h4>
+         </section>
+      );
+   }
+
+   const getResults = response.data.results;
+   if (getResults.length < 1) {
+      return (
+         <section className="image-container">
+            <h4>Results does not exist 🥴.</h4>
+         </section>
+      );
+   }
+
+   return (
+      <section className="image-container">
+         {getResults.map((item) => {
+            const url = item?.urls?.regular;
+            return (
+               <img
+                  src={url}
+                  key={item.id}
+                  alt={item.alt_description}
+                  className="img"
+               />
+            );
+         })}
+      </section>
+   );
 };
 
 export default Gallery;
--- pictures/src/SearchForm.jsx
+++ pictures/src/SearchForm.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { useGlobalContext } from './context';
 
 const SearchForm = () => {
+   const { setSearchTerm } = useGlobalContext();
    const handleSubmit = (e) => {
       e.preventDefault();
       const searchValue = e.target.elements.search.value;
       if (!searchValue) return;
-      console.log(searchValue);
+      setSearchTerm(searchValue);
    };
    return (
       <section>
-         <h1 className="title">unsplash images</h1>
+         <h1 className="title">Geeky Snaps</h1>
          <form className="search-form" onSubmit={handleSubmit}>
             <input
                type="text"
