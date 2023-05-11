ab91445d36c547db3742e9986079170ca4bb36ed
Derryck <oseiagyemanb@gmail.com>
2023-05-11 08:20:09 +0100

    initial commit

---
+++ cocktailDB/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+   return (
+      <section className="section about-section">
+         <h1 className="section-title">about page</h1>
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+            odio sint eius modi, adipisci corporis maxime officiis sunt
+            accusamus incidunt labore recusandae enim doloremque eligendi ipsam
+            et maiores atque mollitia.
+         </p>
+      </section>
+   );
+};
+
+export default About;
